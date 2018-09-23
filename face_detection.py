9e92622f32c6ba4b5ec799ef5ebf0aaddc0a95b9
kimseongjung <plznw4me@naver.com>
2018-09-23 19:20:27 +0900

    with aug

--- face_detection.py
+++ face_detection.py
@@ -13,7 +13,7 @@ test = cv2.imread('waldo_small.png')
 # Convert RGB ==> Greys
 gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
 #load cascade classifier training file for haarcascade
-haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/haarcascade_frontalface_alt.xml')
+haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/lbpcascade_frontalface.xml')
 faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
 
 # print the number of faces found
