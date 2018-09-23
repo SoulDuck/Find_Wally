b09ee6ef23c9e2ae4eb23f4fb5df09b477d7a5c2
kimseongjung <plznw4me@naver.com>
2018-09-23 19:38:25 +0900

    with aug

--- face_detection.py
+++ face_detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import os
 #import matplotlib library
 #import matplotlib.pyplot as plt
 #importing time library for speed comparisons of both classifiers
@@ -9,22 +10,46 @@ def convertToRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-test = cv2.imread('waldo_small.jpg')
+dataset_name = 'waldo_small'
+test = cv2.imread('{}.png')
 # Convert RGB ==> Greys
 print np.shape(test)
 gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
 #load cascade classifier training file for haarcascade
 #haar_face_cascade = cv2.CascadeClassifier('opencv/data/lbpcascades/lbpcascade_frontalface.xml')
-haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
 
-faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
 
-# print the number of faces found
-print('Faces found: ', len(faces))
+detect_list=[
+    'haarcascade_eye_tree_eyeglasses.xml',
+    'haarcascade_frontalcatface.xml',
+    'haarcascade_frontalface_alt.xml',
+    'haarcascade_lefteye_2splits.xml',
+    'haarcascade_profileface.xml',
+    'haarcascade_smile.xml',
+    'haarcascade_eye.xml',
+    'haarcascade_frontalface_alt2.xml',
+    'haarcascade_frontalface_default.xml',
+    'haarcascade_licence_plate_rus_16stages.xml',
+    'haarcascade_righteye_2splits.xml',
+    'haarcascade_upperbody.xml',
+    'haarcascade_frontalcatface_extended.xml',
+    'haarcascade_frontalface_alt_tree.xml',
+    'haarcascade_fullbody.xml',
+    'haarcascade_lowerbody.xml',
+    'haarcascade_russian_plate_number.xml',]
 
-#go over list of faces and draw them as rectangles on original colored
-for (x, y, w, h) in faces:
-    cv2.rectangle(test, (x, y), (x+w, y+h), (0, 255, 0), 2)
+for detector in detect_list:
+    detector_name = os.path.splitext(detector)
+    haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/{}'.format(detector))
+    faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
 
-# write image
-cv2.imwrite('find_waldo_small.png', test)
+    # print the number of faces found
+    print('{} Faces found: '.format(detector_name), len(faces))
+
+    if len(faces) >0:
+        #go over list of faces and draw them as rectangles on original colored
+        for (x, y, w, h) in faces:
+            cv2.rectangle(test, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # write image
+        cv2.imwrite('{}_{}.png'.format(detector_name , dataset_name), test)
