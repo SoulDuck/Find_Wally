7680048b222db1e70fd62eba81efd52a8b2e49ca
kimseongjung <plznw4me@naver.com>
2018-09-23 19:02:08 +0900

    with aug

---
+++ face_detection.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+#import matplotlib library
+import matplotlib.pyplot as plt
+#importing time library for speed comparisons of both classifiers
+import time
+
+def convertToRGB(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+test = cv2.imread('test.png')
+# Convert RGB ==> Greys
+gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
+#load cascade classifier training file for haarcascade
+haar_face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
+faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
+
+# print the number of faces found
+print('Faces found: ', len(faces))
--- make_dataset.py
+++ make_dataset.py
@@ -1,2 +1,6 @@
 import numpy as np
+import glob , os
+os = glob.glob(os.path.join('waldo_original') , '*.jpg')
+
+
 
