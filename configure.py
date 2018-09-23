1a5cc8a0fb9344dc353299e381ad4eb3a42681ca
kimseongjung <plznw4me@naver.com>
2018-09-23 20:02:57 +0900

    with aug

--- configure.py
+++ configure.py
@@ -3,3 +3,30 @@ lr = 0.0001
 max_iter= 100000
 ckpt = 100
 n_classes = 2
+
+
+
+detect_list=[
+    'haarcascade_eye_tree_eyeglasses.xml',
+    'haarcascade_frontalcatface.xml',
+    'haarcascade_frontalface_alt.xml',
+    'haarcascade_lefteye_2splits.xml',
+    'haarcascade_profileface.xml',
+    'haarcascade_smile.xml',
+    'haarcascade_eye.xml',
+    'haarcascade_frontalface_alt2.xml',
+    'haarcascade_frontalface_default.xml',
+    'haarcascade_licence_plate_rus_16stages.xml',
+    'haarcascade_righteye_2splits.xml',
+    'haarcascade_upperbody.xml',
+    'haarcascade_frontalcatface_extended.xml',
+    'haarcascade_frontalface_alt_tree.xml',
+    'haarcascade_fullbody.xml',
+    'haarcascade_lowerbody.xml',
+    'haarcascade_russian_plate_number.xml',]
+
+
+facedetect_list=[
+    'haarcascade_eye_tree_eyeglasses.xml',
+    'haarcascade_frontalcatface.xml',
+    'haarcascade_frontalface_alt.xml',]
--- face_detection.py
+++ face_detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+import configure as cfg
 #import matplotlib library
 #import matplotlib.pyplot as plt
 #importing time library for speed comparisons of both classifiers
@@ -19,39 +20,20 @@ gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
 #load cascade classifier training file for haarcascade
 #haar_face_cascade = cv2.CascadeClassifier('opencv/data/lbpcascades/lbpcascade_frontalface.xml')
 
-
-detect_list=[
-    'haarcascade_eye_tree_eyeglasses.xml',
-    'haarcascade_frontalcatface.xml',
-    'haarcascade_frontalface_alt.xml',
-    'haarcascade_lefteye_2splits.xml',
-    'haarcascade_profileface.xml',
-    'haarcascade_smile.xml',
-    'haarcascade_eye.xml',
-    'haarcascade_frontalface_alt2.xml',
-    'haarcascade_frontalface_default.xml',
-    'haarcascade_licence_plate_rus_16stages.xml',
-    'haarcascade_righteye_2splits.xml',
-    'haarcascade_upperbody.xml',
-    'haarcascade_frontalcatface_extended.xml',
-    'haarcascade_frontalface_alt_tree.xml',
-    'haarcascade_fullbody.xml',
-    'haarcascade_lowerbody.xml',
-    'haarcascade_russian_plate_number.xml',]
-
+detect_list = cfg.facedetect_list
 for detector in detect_list:
     detector_name = os.path.splitext(os.path.split(detector)[-1])
     print detector
     haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/{}'.format(detector))
-    faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
 
-    # print the number of faces found
-    print('{} Faces found: '.format(detector_name), len(faces))
-
-    if len(faces) >0:
-        #go over list of faces and draw them as rectangles on original colored
-        for (x, y, w, h) in faces:
-            cv2.rectangle(test, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        # write image
-        cv2.imwrite('{}_{}.png'.format(detector_name , dataset_name), test)
+    scale_factors = list(np.asarray(range(0, 50)) * 0.1)
+    for scale_factor in scale_factors:
+        faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=scale_factor, minNeighbors=5);
+        # print the number of faces found
+        print('{} Faces found: '.format(detector_name), len(faces))
+        if len(faces) >0:
+            #go over list of faces and draw them as rectangles on original colored
+            for (x, y, w, h) in faces:
+                cv2.rectangle(test, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # write image
+                cv2.imwrite('{}_{}_{}.png'.format(scale_factor, detector_name, dataset_name), test)
