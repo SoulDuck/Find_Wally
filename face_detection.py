ecef036930db210aac2bc907c8f3b428698442b3
kimseongjung <plznw4me@naver.com>
2018-09-23 19:23:10 +0900

    with aug

--- face_detection.py
+++ face_detection.py
@@ -9,7 +9,7 @@ def convertToRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-test = cv2.imread('waldo_small.jpg')
+test = cv2.imread('waldo_big.jpg')
 # Convert RGB ==> Greys
 print np.shape(test)
 gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
