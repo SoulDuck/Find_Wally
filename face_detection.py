8563762ee53107ab9ada90c43622559175a82cb8
kimseongjung <plznw4me@naver.com>
2018-09-23 19:26:05 +0900

    with aug

--- face_detection.py
+++ face_detection.py
@@ -14,8 +14,10 @@ test = cv2.imread('waldo_big.png')
 print np.shape(test)
 gray_img = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
 #load cascade classifier training file for haarcascade
-haar_face_cascade = cv2.CascadeClassifier('opencv/data/lbpcascades/lbpcascade_frontalface.xml')
-faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5);
+#haar_face_cascade = cv2.CascadeClassifier('opencv/data/lbpcascades/lbpcascade_frontalface.xml')
+haar_face_cascade = cv2.CascadeClassifier('opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml.xml')
+
+faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5);
 
 # print the number of faces found
 print('Faces found: ', len(faces))
