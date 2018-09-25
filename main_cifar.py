d3d8ddea1ab1fb7684e34a4c0114de00aa4396a6
kimseongjung <plznw4me@naver.com>
2018-09-25 19:38:22 +0900

    update classifier

--- main_cifar.py
+++ main_cifar.py
@@ -7,7 +7,7 @@ from utils import show_progress
 from eval import Eval
 from aug import aug_lv3
 
-from utils import plot_images
+from utils import plot_images , cls2onehot
 from cifar_input import *
 if __name__ == '__main__':
     # dp == DataProvider
@@ -19,10 +19,15 @@ if __name__ == '__main__':
     train_imgs , train_labs = get_images_labels(*train_filenames)
     test_imgs, test_labs = get_images_labels(*test_filenames)
 
+    train_labs = cls2onehot(train_labs, depth=10)
+    test_labs = cls2onehot(test_labs, depth=10)
+
     # Setting models
     models = Models(n_classes = 10 , img_shape = (64,64,3))
     # Get batch xs , ys
     batch_xs , batch_ys = next_batch(train_imgs ,test_labs ,60)
+
+
 
 
     # Augmenatation
@@ -37,6 +42,7 @@ if __name__ == '__main__':
         train_cost = models.training(batch_xs , batch_ys , cfg.lr)
         if step % cfg.ckpt  == 0 :
             print 'Validation ... '
+
             pred_op, pred_cls, eval_cost, accuracy = models.eval(test_imgs, test_labs)
             #pred_op, pred_cls, eval_cost, accuracy = models.eval(dp.val_imgs , dp.val_labs,)
             #acc = eval.get_acc(sess_op=models.sess, preds_op=models.pred[:,0], batch_size=60, x_op=models.x_,
