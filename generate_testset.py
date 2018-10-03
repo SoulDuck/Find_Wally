04b512019f141e1d6694e59ea421557fa8c82e89
kimseongjung <plznw4me@naver.com>
2018-10-03 16:50:26 +0900

    200 ==> 32

--- generate_testset.py
+++ generate_testset.py
@@ -2,22 +2,22 @@
 
 
 """
-2018.10.2 일날 rasp 로 찍은 월리 사진
-데이터 셋 만드는 법
+2018.10.2 일날 rasp 로 찍은 월리 사진을 데이터 셋에 넣음
 
-
- 
 """
-second_dir='/Users/seongjungkim/Desktop/wally_dataset/second_dataset'
-thrid_dir = '/Users/seongjungkim/Desktop/wally_dataset/third_dataset'
-root_save_dir = 'wally_raspCam_np'
-from image_processing import ImageProcessing
 import glob , os
+from image_processing import ImageProcessing
 import numpy as np
 from PIL import Image
 import utils
-img_prc = ImageProcessing()
 
+root_root_dir = '/mnt/Find_Wally/wally_dataset'
+second_dir=os.path.join( root_root_dir ,'/second_dataset')
+thrid_dir = os.path.join(root_root_dir , 'third_dataset')
+root_save_dir = 'wally_raspCam_np'
+
+
+img_prc = ImageProcessing()
 sec_paths = glob.glob(os.path.join(second_dir , '*.jpg'))
 trd_paths = glob.glob(os.path.join(thrid_dir , '*.jpg'))
 
